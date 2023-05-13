75ed676f3b754a79b95a72bb94366b00dc672ce8
Gideon  Addo <gideonaddoofori@gmail.com>
2023-05-13 08:29:29 +0000

    Add metadata title and description

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,10 @@
 import { EmptyState } from "@/components/empty-state";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User",
+  description: "A list of users in your circle",
+};
 
 export default function Users() {
   return (
