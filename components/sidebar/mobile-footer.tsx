dc54f570e65105aae3357be1cc123b212789d11c
Gideon  Addo <gideonaddoofori@gmail.com>
2023-05-15 21:56:46 +0000

    Add modal controller function

--- components/sidebar/mobile-footer.tsx
+++ components/sidebar/mobile-footer.tsx
@@ -2,17 +2,29 @@
 
 import { useRoutes, useConversation } from "@/hooks";
 import { Item } from "./item";
+import { Avatar } from "../avatar";
+import { User } from "@prisma/client";
+import useSettingsModal from "@/hooks/useSettingsModal";
 
-export function MobileFooter() {
+export function MobileFooter({ currentUser }: { currentUser: User }) {
   const routes = useRoutes();
   const { isOpen } = useConversation();
+  const { open } = useSettingsModal();
 
   if (isOpen) {
     return null;
   }
 
   return (
-    <div className="fixed bottom-0 z-40 flex w-full items-center justify-between border-t bg-background lg:hidden">
+    <div className="fixed bottom-0 z-40 flex w-full items-center justify-between border-t bg-background px-3 py-2 lg:hidden">
+      <button
+        type="button"
+        aria-label="Open menu"
+        onClick={() => open()}
+        className="transition-opacity hover:opacity-75"
+      >
+        <Avatar user={currentUser} />
+      </button>
       {routes.map((route) => (
         <Item
           label={route.label}
